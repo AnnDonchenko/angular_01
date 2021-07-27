e0a8aa0f079e0fe858b4e1d97c7a870cda34576b
novik <annie5donchenko@gmail.com>
2021-07-27 18:39:28 +0300

    lesson_02 done

--- mainTasks/src/app/app.module.ts
+++ mainTasks/src/app/app.module.ts
@@ -1,13 +1,24 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UserPostsComponent } from './components/user-posts/user-posts.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import {routes} from "./routes/users-routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserPostsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
