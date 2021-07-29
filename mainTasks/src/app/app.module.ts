acd093eaa3bb80a60bb0d42a32ab471eca4d6aae
novik <annie5donchenko@gmail.com>
2021-07-29 18:22:31 +0300

    adding files

--- mainTasks/src/app/app.module.ts
+++ mainTasks/src/app/app.module.ts
@@ -1,13 +1,30 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
+import {PostDetailsComponent} from './components/post-details/post-details.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import {routes} from "./routes/basic-routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent,
+    PostDetailsComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mainTasks/src/app/services/post.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IPost} from "../models/ipost";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private httpClient: HttpClient) { }
+
+  getAllPosts(): Observable<IPost[]>{
+    return this.httpClient.get<IPost[]>(this.url)
+  }
+}
